880411800eac0f0abe9518f34e2a58f7d2c7cded
ElMati-01 <129073952+ElMati-01@users.noreply.github.com>
2023-05-11 18:58:00 -0500

    Carpeta practica

---
+++ Ejercicio.py/Ejemplo.py/Untitled-1.py
@@ -0,0 +1,17 @@
+# sueldo = float(input("Ingrese el monto de su salario actual:"))
+# bono= sueldo+sueldo*0.1
+# sueldo_total= bono+sueldo
+# descuento= sueldo-sueldo*0.2
+
+
+# if sueldo > 1000:
+#    print("Su sueldo es:", sueldo_total)
+
+# if sueldo < 5000:
+#   print("Su descuento es de:", descuento)
+
+
+
+
+
+  
---
+++ Ejercicio.py/Ejemplo.py/ejecrico2.py
@@ -0,0 +1,239 @@
+
+#n = int(input("Ingresa un número:"))
+#print(n >= 100)
+# Se leen dos números
+#number1 = int(input("Ingresa el primer número: "))
+#number2 = int(input("Ingresa el segundo número: "))
+
+# Elige el número más grande
+#if number1 > number2: larger_number = number1
+#else: larger_number = number2
+
+# Imprime el resultado
+#print("El número más grande es:", larger_number)
+
+#numero1 = int(input("Ingrese numero: "))
+#numero2 = int(input("Ingrese numero: "))
+#numero3 = int(input("Ingrese numero: "))
+
+#numero_largo = numero1
+
+#if numero2 > numero_largo:
+    #numero_largo = numero2
+    
+#if numero3 > numero_largo:
+    #numero_largo = numero3
+    
+#print("El numero mayor es: ", numero_largo)
+
+##########################
+#name = input("Introduce nombre: ")
+
+#if name == "ESPATIFILIO":
+ #   print("Si - ¡El Espatifilo! es la mejor planta de todos los tiempos!")
+#elif name == "espatifilio":
+ #   print("su mamá hpta")
+#else:
+   #("Espatalitio,  no:", name )
+
+
+# i = 2
+# while i <= 30:
+#      print(i)
+#      i += 1
+
+# for i in range(2,33,2):
+#     print("El numero es: ", i)
+
+
+# for i in range(20):
+#     i+=1
+#     print("El numero es: ", i)
+# print("La suma es: ", 1*10)
+
+##################
+# Almacena el actual número más grande aquí.
+# largest_number = -999999999
+
+# # Ingresa el primer valor.
+# number = int(input("Introduce un número o escribe -1 para detener: "))
+
+# # Si el número no es igual a -1, continuaremos
+# while number != -1:
+#     # ¿Es el número más grande que el valor de largest_number?
+#     if number > largest_number:
+#         # Sí si, se actualiza largest_number.
+#         largest_number = number
+#     # Ingresa el siguiente número.
+#     number = int(input("Introduce un número o escribe -1 para detener: "))
+
+# # Imprime el número más grande.
+# print("El número más grande es:", largest_number)
+# ####
+# identificador de numeros pares: if number % 2 == 1:
+############## Counter ########
+# counter = 5
+# while counter != 0:
+#     print("Dentro del bucle.", counter)
+#     counter -= 1
+# print("Fuera del bucle.", counter)
+####################
+
+####################################### Bucle Mago #####
+
+# secret_number = 777
+# resultado= int(input("Ingresa un numero entero: "))
+
+# print(
+# """
+# +================================+
+# | ¡Bienvenido a mi juego, muggle!|
+# | Introduce un número entero     |
+# | y adivina qué número he        |
+# | elegido para ti.               |
+# |¿Cuál es el número secreto?     |
+# +================================+
+# """)
+
+# while resultado != secret_number:
+#  print("¡Ja, ja! ¡Estás atrapado en mi bucle!")
+ 
+ 
+# if resultado == 777:
+#    print("¡Bien hecho, muggle! Eres libre ahora.")
+
+##################### Contar #######################
+# print("Jugaremos a las escondidas")
+# import time
+
+# for expo in range(1,6):# Escribe un bucle for que cuente hasta cinco.
+#     print(expo, "Mississippi")# Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+#     time.sleep(1)# Cuerpo del bucle, emplea : time.sleep(1)
+
+# print("¡Listos o no, ahí voy!")#
+
+# break - ejemplo
+# ################################### break y continue #######################################
+# print("La instrucción break:")
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print("Dentro del bucle.", i)
+# print("Fuera del bucle.")
+
+
+# # continue - ejemplo
+
+# print("\nLa instrucción continue:")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print("Dentro del bucle.", i)
+# print("Fuera del bucle.")
+
+# resultado= input("Ingrese una palabra: ")
+# chupacabra="chupacabra"
+
+#  while resultado == chupacabra:
+#      print("Eso es todo")
+#      break
+#  if resultado != chupacabra:
+#      print("dentro del bucle")
+
+
+########## Listas ##########
+ 
+# n= [1,2,3,4,5,6,7,8,9]
+
+# print(n[-1])
+# print(n[-3])
+
+
+
+
+
+
+################---------------------------------  ejercicios-----------------------------------------##########################
+#-------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#------- ejercicio 1 --------#
+
+# Nombre= input("ingrese su nombre: ")
+# Edad=input("Ingrese su edad: ")
+# Ciudad= input("Ingrese su ciudad de residencia: ")
+
+# print("\nSus datos a continuación: " "\nSu nombre es: ", Nombre,"\nSu Edad es: ", Edad, "\nSu ciudad de residancia es: ", Ciudad )
+
+# -------------- ejercicio 2 ----------------#
+
+# Dias_trabajados= int(input("Ingrese el numero de dias trabajados: "))
+# Valor_dias =80000
+
+# print("Su paga es de: ", Dias_trabajados * Valor_dias)
+
+# ---------------------------------------- ejercicio 3 -------------------------------#
+
+# print("Bienvenido a la calculadora:" "\n")
+
+# num1= float(input("Ingrese el primer numero: "))
+# num2= float(input("Ingrese el segundo numero"))
+# print("La suma es: ", num1 + num2)
+# print("La resta es: ", num1 - num2)
+# print("La multiplicación es: ", num1 * num2)
+# print("La división es: ", num1 / num2)
+
+#------------------------------------------ ejecicio 4 -----------------------------------------#
+
+# monto= float(input("Ingrese el monto de su salario actual:"))
+# descuento= monto-monto*0.5
+# print("Su descueto es del 50%", descuento)
+
+# ----------------------------------------- ejercicio 5 ----------------------------------------#
+
+# ninos= 50
+# ninas= 20
+# total_ninos= ninos*100/70
+# total_ninas= ninas*100/70
+
+
+# print(f"Porcentaje niños: {total_ninos:.1f}""%")
+# print(f"Porcentaje niñas: {total_ninas:.1f}""%")
+
+#---------------------------------------- ejercicio 6 ------------------------------------------#
+
+# import math
+
+# r= float(input("Ingrese el radio: "))
+# h= float(input("Ingrese la altura: "))
+# Area = 2*math.pi*r*(r+h)
+# Volumen= math.pi*r**2*h
+
+# print("Area: ",Area)
+# print("Volumen ", Volumen)
+
+#--------------------------------------- ejercicio 7 -------------------------------------------#
+
+
+# Valor_factura= float(input("Ingrese el valor de la factura: "))
+# IVA= 0.19
+# Valor_final= Valor_factura + Valor_factura * IVA
+
+# print("EL monto del IVA es del 19%, por lo tanto el valor total es:", Valor_final)
+
+#--------------------------------------- ejercicio 8 -------------------------------------------#
+
+nota1 = float(input)
+
+
+
+
+
+
+
+
+
+
+
+
